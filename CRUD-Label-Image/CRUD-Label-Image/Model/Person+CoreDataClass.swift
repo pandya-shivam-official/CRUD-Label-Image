179f0e58fc6541eff407fe49111966e40b83a4d3
Shivam Pandya <126809212+pandya-shivam-official@users.noreply.github.com>
2023-04-28 17:42:01 +0530

    Add files via upload

---
+++ CRUD-Label-Image/CRUD-Label-Image/Model/Person+CoreDataClass.swift
@@ -0,0 +1,15 @@
+//
+//  Person+CoreDataClass.swift
+//  CRUD-Label-Image
+//
+//  Created by Shivam Pandya on 28/04/23.
+//
+//
+
+import Foundation
+import CoreData
+
+@objc(Person)
+public class Person: NSManagedObject {
+
+}
